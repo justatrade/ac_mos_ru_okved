b2515bd67454564c71675b784a49178593c29603
justatrade <justatrade@mail.ru>
2025-10-31 12:20:10 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,192 @@
+import json
+import re
+from pprint import pprint
+from typing import Dict
+
+import phonenumbers
+import requests
+
+json_url = (
+    "https://gist.githubusercontent.com/dmitry-naumenko/"
+    "5a8afef9d94e9bcf9d91052dd65089ab/raw/"
+    "c19ebdf3594e60db183cc2a9be9a7996a63dbf66/"
+    "%25D0%25BE%25D0%25BA%25D0%25B2%25D1%258D%25D0%25B4.json"
+)
+matches = {}
+
+
+def normalize_russian_phone(phone_number: str) -> str | None:
+    """
+    Get phone string at any format and trying to convert to valid
+    Russian phone number
+    :param phone_number:
+    :return: Correctly formatted phone number or None if phone number is invalid
+    """
+    if not isinstance(phone_number, str):
+        return None
+
+    # Getting any digits from a string, ignoring all any other symbols
+    digits = re.sub(r'\D', '', phone_number)
+
+    if len(digits) == 10:
+        digits = "7" + digits
+    elif len(digits) == 11:
+        if digits[0] == "8":
+            digits = "7" + digits[1:]
+        elif digits[0] != "7":
+            return None
+    else:
+        return None
+
+    if len(digits) == 11 and digits[0] == "7":
+        return '+' + digits
+
+    return None
+
+
+def get_phone_number() -> str | None:
+    """
+    Get phone number at any format and trying to convert to valid
+    :return: Phone number in Russian format or None if no correct phone number
+    """
+    raw_number = input("Input phone number: ")
+
+    normalized = normalize_russian_phone(raw_number)
+    if not normalized:
+        print("Please enter a valid phone number")
+
+        return
+
+    phone_number = phonenumbers.parse(normalized)
+
+    if phonenumbers.is_valid_number(phone_number):
+        print("Correct phone number")
+
+        return raw_number
+
+    else:
+        print("Incorrect phone number")
+
+    return
+
+
+def get_okved_base() -> list[Dict]:
+    """
+    Getting remote json based list of all OKVEDs
+    :return:
+    """
+    raw_json = requests.get(
+        url=json_url,
+    )
+    try:
+        okved_base: list[Dict] = raw_json.json()
+    except json.decoder.JSONDecodeError:
+        return []
+
+    return okved_base
+
+
+def tree_search(tree: list[Dict], target: str, fallback: bool = False) -> Dict:
+    """
+    Searching our json tree recursively
+    :param tree: Tree to search
+    :param target: Target string to search for
+    :param fallback: Fallback searching mode
+    :return:
+    """
+    for each in tree:
+        if each.get("items"):
+            # Searching recursively for items
+            matches.update(tree_search(each["items"], target, fallback))
+
+        # Searching in a base element, cos it is having a code also
+        result: Dict[str, str] = linear_search(each, target, fallback)
+
+        if result:
+            # As long as codes are unique, we could use them directly as keys
+            matches[result["code"]] = result["name"]
+
+    return matches
+
+
+def linear_search(
+        current_items: Dict[str, str],
+        target: str,
+        fallback
+) -> Dict[str, str]:
+    """
+    Searching the exact category, ignoring 'items' element
+    :param current_items: Dict with current items
+    :param target: Target string to search for
+    :param fallback: Fallback searching mode
+    :return:
+    """
+    result = {}
+    raw_code = current_items["code"]
+    code = "".join(raw_code.split("."))
+
+    if not fallback:
+        if code == target[-len(code):]:
+            result = {
+                "code": current_items["code"],
+                "name": current_items["name"],
+            }
+    else:
+        # In case of fallback, using Knuth-Morris-Pratt algorithm
+        if kmp(code, target) > 0:
+            result = {
+                "code": current_items["code"],
+                "name": current_items["name"],
+            }
+
+    return result
+
+
+def prefix(s: str) -> list[int]:
+    """
+    Prefix function for KMP algorithm
+    :param s: String to prefix
+    :return:
+    """
+    v = [0]*len(s)
+    for i in range(1, len(s)):
+        k = v[i-1]
+        while k > 0 and s[k] != s[i]:
+            k = v[k-1]
+        if s[k] == s[i]:
+            k = k + 1
+        v[i] = k
+    return v
+
+
+def kmp(s: str, t: str) -> int:
+    """
+    Knuth-Morris-Pratt algorithm
+    :param s: String to search for
+    :param t: String to search in
+    :return: Starting index of searched string or -1
+    """
+    index = -1
+    f = prefix(s)
+    k = 0
+    for i in range(len(t)):
+        while k > 0 and s[k] != t[i]:
+            k = f[k-1]
+        if s[k] == t[i]:
+            k = k + 1
+        if k == len(s):
+            index = i - len(s) + 1
+            break
+    return index
+
+
+if __name__ == '__main__':
+    phone = get_phone_number()
+    if not phone:
+        exit(-1)
+    base = get_okved_base()
+    simple_search = tree_search(base, phone)
+    if not simple_search:
+        print("No exact suffix matches found, doing fallback search")
+        tree_search(base, phone, fallback=True)
+    pprint(matches)
